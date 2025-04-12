c6ba38369d026fb0685a54059ebef8bbcc73348f
dyune <david.wang5@mail.mcgill.ca>
2025-04-11 20:02:52 -0400

    Refactor: pong

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -3,7 +3,10 @@ package api
 import "github.com/gofiber/fiber/v2"
 
 func TestRoutes(app *fiber.App) {
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	app.Get("/ping", ping)
+
+}
+
+func ping(c *fiber.Ctx) error {
+	return c.SendString("pong")
 }
